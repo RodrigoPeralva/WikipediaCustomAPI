538b6f2ff276379bc1dd4114dc9b34f370887c06
RodrigoPeralva <rodrigopmachado2012@gmail.com>
2021-02-22 20:58:56 -0300

    Adicionando API de consumo da WikipediaAPI.

---
+++ WikipediaAPI/Controllers/WikipediaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace WikipediaAPI.Controllers {
+
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class WikipediaController : ControllerBase {
+
+        [HttpGet]
+        public async Task<IActionResult> GetByWord(string palavra) {
+            WikipediaClient wClient = new WikipediaClient();
+
+            var result = await wClient.GetWiki(palavra);
+
+            return Ok(result);
+        }
+    }
+}
---
+++ WikipediaAPI/WikipediaClient.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WikipediaAPI {
+
+    public class WikipediaClient {
+        private readonly HttpClient client = new HttpClient();
+        private const string baseUrl = "https://en.wikipedia.org/w/api.php";
+
+        public Task<string> GetWiki(string palavra) {
+            var paramsUrl = $"?action=query&format=json&list=search&srsearch={palavra}";
+
+            var url = baseUrl + paramsUrl;
+
+            var res = client.GetStringAsync(url);
+            return res;
+        }
+    }
+}
